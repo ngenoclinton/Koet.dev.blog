2fce7eb55183f7b070494c64a3f98066fa2f21f6
ngenoclinton <clintonkipkoechngeno@gmail.com>
2025-10-23 17:39:02 +0300

    changes

--- src/app/blog/[category]/page.tsx
+++ src/app/blog/[category]/page.tsx
@@ -6,40 +6,35 @@ import Header from "@/components/Header";
 import CardCategory from "@/components/CardCategory";
 import NotFound from "@/components/NotFound";
 
-export async function generateStaticParams() {
-  let posts = getBlogPosts();
+export function generateStaticParams() {
+  const posts = getBlogPosts();
   return posts.map((post) => ({
     category: post.metadata.category,
   }));
 }
 
 export function generateMetadata({ params }: { params: { category: string } }) {
-  let { category } = params;
+  const { category } = params;
 
   return {
-    title: category.toLocaleUpperCase(),
-    description: `All articles reagarding ${category}`,
+    title: category.toUpperCase(),
+    description: `All articles regarding ${category}`,
   };
 }
 
-export default function Page({ params }: { params: { category: string } }) {
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ category: string }>;
+}) {
+  const { category } = await params;
 
-  let posts = getBlogPosts().filter(
-    (post) => post.metadata.category === params.category
+  const posts = getBlogPosts().filter(
+    (post) => post.metadata.category === category
   );
-
-//-----------------------------------
-let postMtDt = getBlogPosts().filter(
-    (post) => post.metadata
-  );
-
-let postSlug = getBlogPosts().filter(
-    (post) => post.slug
-  );
-//------------------------------------
 
   if (!posts.length) {
-    <NotFound />
+    return <NotFound />;
   }
 
   return (
@@ -47,22 +42,19 @@ let postSlug = getBlogPosts().filter(
       <Header>
         <Container>
           <h1 className="title font-semibold text-2xl tracking-wider mt-4 uppercase">
-            {posts[0]?.metadata.category}
+            {category}
           </h1>
         </Container>
       </Header>
+
       <Container>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10">
           {posts
-            .sort((a, b) => {
-              if (
-                new Date(a.metadata.publishedAt) >
-                new Date(b.metadata.publishedAt)
-              ) {
-                return -1;
-              }
-              return 1;
-            })
+            .sort(
+              (a, b) =>
+                new Date(b.metadata.publishedAt).getTime() -
+                new Date(a.metadata.publishedAt).getTime()
+            )
             .map((post) => (
               <Link
                 href={`/blog/${post.metadata.category}/${post.slug}`}
